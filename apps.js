12fce672f561b6d3b934824c3d8340f5ce96bd4c
Unko <kobie.watkins@gmail.com>
2020-07-25 21:17:08 -0400

    the apps.js file is tested and works, not completed yet-few more lines of code to complete and a rendercode

--- apps.js
+++ apps.js
@@ -11,8 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 //team members json object
-
-const employees = [];
+var team = [];
 
 //function call to initialize
 
@@ -36,135 +35,149 @@ async function promptUser() {
     });
 
     //Build an Engineering Team
-    getManagerdata();
+    getManagerInfo();
 
-    function getManagerdata() {
-    prompt([
-        {
-          type:"input",
-          name: "managerName",
-          message: "Enter mangers name please",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Employee Id number please?",
-          validate: function (id) {
-            const idValid = /^[1114-3009]+$/.test(id)
-                if (idValid) {
-                    console.log("Awesome, Thanks")
-                    return true;
+    function getManagerInfo() {
+      console.log("build the HR Team");
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "managerName",
+            message: "Enter mangers name please",
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "Employee Id number please?",
+          },
+          {
+            type: "input",
+            name: "mangerEmail",
+            message: "Enters managers email",
+          },
+          {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "Enter the managers office number",
+          },
+        ])
+        .then(function (answers) {
+          const manager = new Manager(
+            answers.managerName,
+            answers.managerId,
+            answers.managerEmail,
+            answers.managerOfficeNumber
+          );
+          team.push(manager);
+          buildTeam();
+        });
+
+      function addEngineer() {
+        prompt([
+          {
+            type: "input",
+            name: "engineerName",
+            message: "Enter the engineers name",
+          },
+          {
+            type: "input",
+            name: "engineerid",
+            message: "Enter engineers Id",
+          },
+          {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is your email",
+          },
+          {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is your Github username",
+          },
+        ]).then(function (answers) {
+          const engineer = new Engineer(
+            answers.engineerName,
+            answers.engineerId,
+            answers.engineerEmail,
+            answers.engineerGithub
+          );
+          team.push(engineer);
+          buildTeam();
+        });
+      }
+
+      function addIntern() {
+        prompt([
+          {
+            type: "input",
+            name: "internName",
+            message: "Please enter the intern's name",
+          },
+          {
+            type: "input",
+            name: "internId",
+            message: "Enter the intern's id",
+          },
+          {
+            type: "input",
+            name: "internEmail",
+            message: "Enter the intern's email",
+          },
+          {
+            type: "input",
+            name: "internSchool",
+            message: "Enter the intern's educational institution",
+          },
+        ]).then(function (answers) {
+          const intern = new Intern(
+            answers.internName,
+            answers.internId,
+            answers.internEmail,
+            answers.internSchool
+          );
+          team.push(intern);
+          buildTeam();
+        });
+      }
+
+      function buildTeam() {
+        prompt([
+          {
+            type: "list",
+            name: "teamRoles",
+            choices: ["Manager", "Intern", "Engineer", "None"],
+            message: "Please choose the team member's role.",
+          },
+        ]).then(function (answers) {
+          switch (answers.teamRoles) {
+            case "Intern":
+              addIntern();
+              break;
+            case "Engineer":
+              addEngineer();
+              break;
+            default:
+              generateTeam();
           }
-          else{
-            console.log(".please enter a new Id; choice not valid")
-            return false;
-          }
-        },
-      },
-      {
-        type: "input",
-        name: "mangeremail",
-        message: "Enters managers email",
-
-      },
-      {
-        type: "input",
-        name: "managersOfficeNumber",
-        message: "Enter the managers office number.",
+        });
       }
-    ]).then(answers => {
-      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-      teamMembers.push(manager);
-  });
-}
-
-  function addEngineer() {
-    prompt([
-
-    {
-      type: "input",
-      name: "engineerName",
-      message: "Enter the engineers name",
-    },
-    {
-      type: "input",
-      name: "engineerid",
-      message: "Enter engineers Id",
-      validate: function (id) {
-        const idValid = /^[1114-3009]+$/.test(id)
-            if (idValid) {
-                console.log("Awesome, Thanks")
-                return true;
-      }
-      else{
-        console.log(".please enter a new Id; choice not valid")
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "engineerEmail",
-    message: "What is your email",
-  },
-  {
-    type: "input",
-    name: "engineerGithub",
-    message: "What is your Github username"
-  }
-]).then(function(answers) {
-  const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
-  team.push(engineer)
-  buildTeam()
-})
-}
-function addIntern() {
-  prompt([
-  {
-      type: "input",
-      name: "internName",
-      message: "Please enter the intern's name",
-  },
-  {
-      type: "input",
-      name: "internId",
-      message: "Enter the intern's id",
-      validate: function (id) {
-        const idValid = /^[1114-3009]+$/.test(id)
-            if (idValid) {
-                console.log("Awesome, Thanks")
-                return true;
-      }
-      else{
-        console.log(".please enter a new Id; choice not valid")
-        return false;
-      }
-    },
-  },
-  {
-      type: "input",
-      name: "internEmail",
-      message: "Enter the intern's email",
-  },
-  {
-      type: "input",
-      name: "internSchool",
-      message: "Enter the intern's educational institution",
-  }
-]).then(function(answers) {
-  const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
-  team.push(intern)
-})
-}
-
-
-});
-
-    
+    }
   } catch (err) {
-    console.log(err); //required to complete
+    console.log(err); // log error if try does not complete
   }
 }
+
+// function generateTeam() {
+//   const employees = render(team);
+//   fs.writeFile(outputPath, employees, function(err) {
+//       if(err){
+//           console.log(err);
+//       }
+//           console.log("Data entered!");
+//   })
+
+// };
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
